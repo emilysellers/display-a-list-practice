1306122be0dd903b8dc8afaab3d3dad36cbb9adb
Emily Sellers <emilymelindasellers@gmail.com>
2022-09-15 13:08:11 -0700

    correct js typo

--- cars-data.js
+++ cars-data.js
@@ -1,17 +1,17 @@
-export const cars = [;
-{
-    make: 'Toyota',
-    model: 'Tacoma',
-    type: 'pickup truck',
-},
-{
-    make: 'Volkswagon',
-    model: 'Westfalia',
-    type: 'camper van',
-},
-{
-    make: 'Toyota',
-    model: 'Prius',
-    type: 'hybrid sedan',
-}
+export const cars = [
+    {
+        make: 'Toyota',
+        model: 'Tacoma',
+        type: 'pickup truck',
+    },
+    {
+        make: 'Volkswagon',
+        model: 'Westfalia',
+        type: 'camper van',
+    },
+    {
+        make: 'Toyota',
+        model: 'Prius',
+        type: 'hybrid sedan',
+    },
 ];
